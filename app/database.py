e3693846bae2db53f3ce1f1da6cc4c83d01c8623
Hikaru Kono <hikaruk20010806@icloud.com>
2025-09-29 14:41:33 +0900

    sqliteに変更

--- app/database.py
+++ app/database.py
@@ -1,9 +1,21 @@
+# MySQL用の設定
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://fastapi_user:fastapi_pass@db:3306/fastapi_db"
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
+
+
+# SQLite用の設定
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://fastapi_user:fastapi_pass@db:3306/fastapi_db"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
